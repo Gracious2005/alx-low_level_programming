e390ace075573bd4c8876f58b7f71e051ed3a7e0
Gracious2005 <graciousogooluwa005@gmail.com>
2023-07-05 13:09:28 -0700

    Singly Linked List

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,24 +6,24 @@
  * add_node - add new node
  * @head: double pointer
  * @str: new string
+ * Return: NULL if failed otherwise new address of new element
  */
 list_t *add_node(list_t **head, const char *str)
 {
-       list_t *new;
- unsigned int length = 0;
+	list_t *new;
+	unsigned int length = 0;
 
- while (str[length])
- length++;
+	while (str[length])
+		length++;
 
- new = malloc(sizeof(list_t));
- if (!new)
- return (NULL);
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
 
- new->str = strdup(str);
- new->len = len;
- new->next = (*head);
- (*head) = new;
+	new->str = strdup(str);
+	new->len = length;
+	new->next = (*head);
+	(*head) = new;
 
- return (*head);
+	return (*head);
 }
-
