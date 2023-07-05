132f9674ef1f6a1c980dacfa757724c54732facf
Gracious2005 <graciousogooluwa005@gmail.com>
2023-07-05 12:42:07 -0700

    Singly linked List

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	size_t nodeCount = 0;
 
 	while (h)
 	{
 		if (!h->str)
 		printf("[0] (nil)\n");
 		else
-		printf("[%u] %s\n", h->length, h->str);
+		printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		s++;
+		nodeCount++;
 	}
-	return (s);
+	return nodeCount;
 }
