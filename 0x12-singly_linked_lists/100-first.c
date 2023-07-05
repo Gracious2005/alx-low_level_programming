f35cb7d5204c8b7a792d054647c583a7728d958e
Gracious2005 <graciousogooluwa005@gmail.com>
2023-07-05 13:14:57 -0700

    Singly Linked List

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,10 @@
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before the main
- * function is executed
+ * first - Write a function that prints You're beat!
+ * and yet, you must allow,\nI bore my
+ * house upon my back!\n before the
+ * main function is executed.
  */
 void first(void)
 {
